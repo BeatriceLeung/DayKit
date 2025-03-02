6b68aa60102e58cc522e87bd1d4262632bb920b9
BeatriceLeung <bbeatrice.leung@gmail.com>
2025-03-02 12:00:48 -0800

    Integrated weather module

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from openai import OpenAI
 import os
 # from flask_cors import CORS
+import weather_url as WeatherAPI
 
 app = Flask(__name__)
 # CORS(app, supports_credentials=True)
@@ -10,18 +11,23 @@ client = OpenAI(api_key=os.environ.get('API_KEY'))
 
 @app.route("/")
 def hello_world():
+    temp = WeatherAPI.getTemperature()
+    min_temp = temp["min"]
+    max_temp = temp["max"]
+    cur_temp = temp["current"]
+
     completion = client.chat.completions.create(
     model="gpt-4o-mini",
     messages=[
         {"role": "system", "content": "You are a helpful assistant."},
         {
             "role": "user",
-            "content": "Recommend me an outfit for 60 degrees Farenheit weather."
+            "content": "Recommend me an outfit for " + str(cur_temp) + "degrees Farenheit weather."
         }
         ],
         temperature=0.7 #controls randomness
     )
-    return "<p>" + completion.choices[0].message.content + "</p>"
+    return "<p>" + completion.choices[0].message.content + "</p> <p>" + str(temp) + "</p>"
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5002)
--- server/weather_url.py
+++ server/weather_url.py
@@ -4,37 +4,43 @@ import requests_cache
 import pandas as pd
 from retry_requests import retry
 
+
 # Setup the Open-Meteo API client with cache and retry on error
 cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
 retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
 openmeteo = openmeteo_requests.Client(session = retry_session)
 
-# Make sure all required weather variables are listed here
-# The order of variables in hourly or daily is important to assign them correctly below
-url = "https://api.open-meteo.com/v1/forecast"
-params = {
-	"latitude": 34.0522,
-	"longitude": -118.2437, # UCLA
-	"current": "temperature_2m",
-	"daily": ["temperature_2m_max", "temperature_2m_min"],
-	"temperature_unit": "fahrenheit",
-	"timezone": "America/Los_Angeles",
-	"forecast_days": 1
-}
-responses = openmeteo.weather_api(url, params=params)
 
-response = responses[0]
+def getTemperature():
+	# Make sure all required weather variables are listed here
+	# The order of variables in hourly or daily is important to assign them correctly below
+	url = "https://api.open-meteo.com/v1/forecast"
+	params = {
+		"latitude": 34.0522,
+		"longitude": -118.2437, # UCLA
+		"current": "temperature_2m",
+		"daily": ["temperature_2m_max", "temperature_2m_min"],
+		"temperature_unit": "fahrenheit",
+		"timezone": "America/Los_Angeles",
+		"forecast_days": 1
+	}
+	responses = openmeteo.weather_api(url, params=params)
 
-# Current values. The order of variables needs to be the same as requested.
-current = response.Current()
+	response = responses[0]
 
-current_temperature_2m = current.Variables(0).Value()
+	# Current values. The order of variables needs to be the same as requested.
+	current = response.Current()
 
-print(f"Current temperature_2m {current_temperature_2m}")
-# Process daily data. The order of variables needs to be the same as requested.
-daily = response.Daily()
-daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
-daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+	current_temperature_2m = current.Variables(0).Value()
 
-print("Daily max(F): ", daily_temperature_2m_max)
-print("Daily min(F): ", daily_temperature_2m_min)
+	print(f"Current temperature_2m {current_temperature_2m}")
+	# Process daily data. The order of variables needs to be the same as requested.
+	daily = response.Daily()
+	daily_temperature_2m_max = daily.Variables(0).ValuesAsNumpy()
+	daily_temperature_2m_min = daily.Variables(1).ValuesAsNumpy()
+
+	print("Daily max(F): ", daily_temperature_2m_max)
+	print("Daily min(F): ", daily_temperature_2m_min)
+
+	return {"current": current_temperature_2m, "min": daily_temperature_2m_min[0],
+		"max" : daily_temperature_2m_max[0]}

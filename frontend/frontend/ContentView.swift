9960e148e1a3387a8147deaee25ad2f6d0e834a3
June Qin <juneqin2017@gmail.com>
2025-03-02 20:53:59 -0800

    Display login view

--- frontend/frontend/ContentView.swift
+++ frontend/frontend/ContentView.swift
@@ -51,18 +51,11 @@ struct ContentView: View {
 
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-            
-        }
-        .padding()
+        LoginView()
     }
 }
 
-/*
+
 #Preview {
     ContentView()
-} */
+}

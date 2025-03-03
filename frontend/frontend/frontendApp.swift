fd90bf2e1db630b0e8e24066574f0b5a13c70d6d
June Qin <juneqin2017@gmail.com>
2025-03-02 16:38:57 -0800

    Add frontend

---
+++ frontend/frontend/frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  frontendApp.swift
+//  frontend
+//
+//  Created by June Qin on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/frontendTests/frontendTests.swift
@@ -0,0 +1,17 @@
+//
+//  frontendTests.swift
+//  frontendTests
+//
+//  Created by June Qin on 3/2/25.
+//
+
+import Testing
+@testable import frontend
+
+struct frontendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
